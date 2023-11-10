b374ee65c3c53e74a763238c97a7c3602054e45c
e.hasa <e.hasa@LOGICAL18>
2023-11-10 10:42:10 +0100

    Update DashboardADMIN

--- LogPark/DashboardAdmin.cs
+++ LogPark/DashboardAdmin.cs
@@ -25,54 +25,9 @@ namespace LogPark
             InitializeComponent();
             string SaveLanguage = Properties.Settings.Default.Language;
             Thread.CurrentThread.CurrentUICulture = new CultureInfo(SaveLanguage);
-
-            this.WindowState = FormWindowState.Normal;
-            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
-            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
-            Resolution objFormResizer = new Resolution();
-            objFormResizer.ResizeForm(this, screenHeight, screenWidth);
-
-
             parkingService = new ParkingService(new ParkingRepository());
 
         }
-
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            Check checkin = new Check();
-            checkin.ShowDialog();
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            CheckOut checkOut = new CheckOut();
-            checkOut.ShowDialog();
-            this.Hide();
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            Reports reports = new Reports();
-            reports.ShowDialog();
-            this.Hide();
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-            this.Hide();
-            SettingsAdmin settingsAdmin = new SettingsAdmin();
-            settingsAdmin.ShowDialog();
-
-         
-        }
-    
-
-
 
 
         private void DashboardAdmin_Load(object sender, EventArgs e)
@@ -90,21 +45,53 @@ namespace LogPark
             }
 
 
-            label5.Text = label5.Text.Replace(" Free Space", "") + $":{freeSpaces} /  {totalParkingSpaces}";
+            label1.Text = label1.Text.Replace(" Free Space", "") + $":{freeSpaces} /  {totalParkingSpaces}";
 
 
         }
-
-        private void label5_Click(object sender, EventArgs e)
+     
+      
+        private void label1_Click_1(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click_2(object sender, EventArgs e)
         {
             this.Hide();
-            Login login = new Login();
-            login.ShowDialog();
+            Check checkin = new Check();
+            checkin.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            CheckOut checkOut = new CheckOut();
+            checkOut.ShowDialog();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Reports reports = new Reports();
+            reports.ShowDialog();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            SettingsAdmin settingsAdmin = new SettingsAdmin();
+            settingsAdmin.ShowDialog();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login Login = new Login();
+            Login.ShowDialog();
         }
     }
 }
